2300b39034680a2ffcb48e067b3bdb5a5c33a335
Christopher Weil <cweil@salesforce.com>
2023-03-08 14:52:56 -0500

    prettified code

--- force-app/main/default/lwc/deviceCard/deviceCard.js
+++ force-app/main/default/lwc/deviceCard/deviceCard.js
@@ -1,90 +1,85 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-import usagePhone from '@salesforce/resourceUrl/UsagePhone';
-import usageTablet from '@salesforce/resourceUrl/UsageTablet';
-import usageWatch from '@salesforce/resourceUrl/UsageWatch';
+import usagePhone from "@salesforce/resourceUrl/UsagePhone";
+import usageTablet from "@salesforce/resourceUrl/UsageTablet";
+import usageWatch from "@salesforce/resourceUrl/UsageWatch";
 
 export default class DeviceCard extends LightningElement {
-    @api asset;
-    @api colorMap;
-    @api type;
+  @api asset;
+  @api colorMap;
+  @api type;
 
-    name;
-    contactNumber;
-    icon;
-    device;
-    usage;
-    unitOfMeasure;
-    totalUsage;
+  name;
+  contactNumber;
+  icon;
+  device;
+  usage;
+  unitOfMeasure;
+  totalUsage;
 
-    nearLimit = false;
-    overLimit = false;
-    
-    renderedCallback() {
-        this.name = this.asset.User__c;
-        this.contactNumber = this.asset.Phone_Number_Display__c;
-        this.device = this.asset.Make_and_Model__c;
-        this.usage = this.asset.Usage__r.Current_Usage__c;
-        this.unitOfMeasure = this.asset.Usage__r.Unit_of_Measure__c;
-        this.setIcon();
-        this.determineUsageLimit();
+  nearLimit = false;
+  overLimit = false;
+
+  renderedCallback() {
+    this.name = this.asset.User__c;
+    this.contactNumber = this.asset.Phone_Number_Display__c;
+    this.device = this.asset.Make_and_Model__c;
+    this.usage = this.asset.Usage__r.Current_Usage__c;
+    this.unitOfMeasure = this.asset.Usage__r.Unit_of_Measure__c;
+    this.setIcon();
+    this.determineUsageLimit();
+  }
+
+  setIcon() {
+    switch (this.asset.Device_Type__c) {
+      case "Phone":
+        this.icon = usagePhone + "#icon";
+        break;
+      case "Tablet":
+        this.icon = usageTablet + "#icon";
+        break;
+      case "Wearable":
+        this.icon = usageWatch + "#icon";
+        break;
+      default:
+        this.icon = "utility:help";
     }
+  }
 
-    setIcon() {
-        switch(this.asset.Device_Type__c) {
-            case 'Phone':
-                this.icon = usagePhone+'#icon';
-                break;
-            case 'Tablet':
-                this.icon = usageTablet+'#icon';
-                break;
-            case 'Wearable':
-                this.icon = usageWatch+'#icon';
-                break;
-            default:
-                this.icon = 'utility:help';
-        }
-    }
-
-    determineUsageLimit() {
-        switch (this.type) {
-            case 'Data':
-                if (this.asset.Unlimited_Data__c) {
-                    this.totalUsage = 'unlimited';
-                } else {
-                    this.totalUsageCalculation();
-                }
-                break;
-            case 'Talk':
-                if (this.asset.Unlimited_Talk__c) {
-                    this.totalUsage = 'unlimited';
-                } else {
-                    this.totalUsageCalculation();
-                }
-                break;
-            case 'Text':
-                if (this.asset.Unlimited_Text__c) {
-                    this.totalUsage = 'unlimited';
-                } else {
-                    this.totalUsageCalculation();
-                }
-                break;
-            default:
-                this.totalUsage = 'error';
-        }
-        if (!this.asset.Unlimited_Data__c) {
-            
+  determineUsageLimit() {
+    switch (this.type) {
+      case "Data":
+        if (this.asset.Unlimited_Data__c) {
+          this.totalUsage = "unlimited";
         } else {
-            this.totalUsage = 'unlimited';
+          this.totalUsageCalculation();
         }
+        break;
+      case "Talk":
+        if (this.asset.Unlimited_Talk__c) {
+          this.totalUsage = "unlimited";
+        } else {
+          this.totalUsageCalculation();
+        }
+        break;
+      case "Text":
+        if (this.asset.Unlimited_Text__c) {
+          this.totalUsage = "unlimited";
+        } else {
+          this.totalUsageCalculation();
+        }
+        break;
+      default:
+        this.totalUsage = "error";
     }
+  }
 
-    totalUsageCalculation() {
-        this.totalUsage = this.asset.Usage__r.Allotted_Usage__c;
-        if (this.usage / this.totalUsage >= 1) {
-            this.overLimit = true;
-        } else if (this.usage / this.totalUsage > 0.9) {
-            this.nearLimit = true;
-        }
+  totalUsageCalculation() {
+    this.totalUsage = this.asset.Usage__r.Allotted_Usage__c;
+    if (this.usage / this.totalUsage >= 1) {
+      this.overLimit = true;
+    } else if (this.usage / this.totalUsage > 0.9) {
+      this.nearLimit = true;
     }
+  }
 }
--- force-app/main/default/lwc/planSection/planSection.js
+++ force-app/main/default/lwc/planSection/planSection.js
@@ -1,94 +1,105 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-const COLORS = ['#96F2EE', '#68CEEE', '#2D9CED', '#0E6ECE', '#073E92', '#051C61'];
+const COLORS = [
+  "#96F2EE",
+  "#68CEEE",
+  "#2D9CED",
+  "#0E6ECE",
+  "#073E92",
+  "#051C61"
+];
 
 export default class PlanSection extends LightningElement {
-    @api plan;
+  @api plan;
 
-    planAssets;
+  planAssets;
 
-    type = 'Data';
-    colorMap = {};
-    expanded = true;
-    icon = "utility:switch";
-    isLoading = true;
+  type = "Data";
+  colorMap = {};
+  expanded = true;
+  icon = "utility:switch";
+  isLoading = true;
 
-    dataButtonVariant = 'Brand';
-    textButtonVariant = 'Neutral';
-    talkButtonVariant = 'Neutral';
+  dataButtonVariant = "Brand";
+  textButtonVariant = "Neutral";
+  talkButtonVariant = "Neutral";
 
-    get hasData() {
-        if(this.plan.Assets__r.length) {
-            return this.plan.Assets__r.length > 0;
-        }
-        return false;
+  get hasData() {
+    if (this.plan.Assets__r.length) {
+      return this.plan.Assets__r.length > 0;
     }
+    return false;
+  }
 
-    connectedCallback(){
-        this.completedDataProcessing(this.plan, this.type);
-    }
+  connectedCallback() {
+    this.completedDataProcessing(this.plan, this.type);
+  }
 
-    @api
-    completedDataProcessing(plan, type) {
-        this.isLoading = false;
-        this.plan = plan;
-        this.planAssets = this.plan.Assets__r;
-        this.planAssets.forEach((asset, index) => {
-            this.colorMap[asset.Id] = COLORS[index % COLORS.length];
+  @api
+  completedDataProcessing(plan, type) {
+    this.isLoading = false;
+    this.plan = plan;
+    this.planAssets = this.plan.Assets__r;
+    this.planAssets.forEach((asset, index) => {
+      this.colorMap[asset.Id] = COLORS[index % COLORS.length];
+    });
+    this.type = type;
+    this.setButtonVariants();
+    setTimeout(() => {
+      this.template
+        .querySelectorAll(".custom-border")
+        .forEach((selector, index) => {
+          selector.style.borderLeftColor = COLORS[index % COLORS.length];
         });
-        this.type = type;
-        this.setButtonVariants();
-        setTimeout(() => {
-            this.template.querySelectorAll('.custom-border').forEach((selector, index) => {
-                selector.style.borderLeftColor = COLORS[index % COLORS.length];
-            });
-            this.template.querySelector('c-pie-chart').createChart(this.planAssets, this.colorMap);
-        }, 0);
-    }
+      this.template
+        .querySelector("c-pie-chart")
+        .createChart(this.planAssets, this.colorMap);
+    }, 0);
+  }
 
-    toggleSection() {
-        this.expanded = !this.expanded;
-        if (this.expanded) {
-            this.icon = "utility:switch";
-            this.completedDataProcessing(this.plan, this.type);
-        } else {
-            this.icon = "utility:chevronup";
-        }
+  toggleSection() {
+    this.expanded = !this.expanded;
+    if (this.expanded) {
+      this.icon = "utility:switch";
+      this.completedDataProcessing(this.plan, this.type);
+    } else {
+      this.icon = "utility:chevronup";
     }
+  }
 
-    setType(event) {
-        this.isLoading = true;
+  setType(event) {
+    this.isLoading = true;
 
-        const typeEvent = new CustomEvent('typeselect', {
-            detail: {
-                plan: this.plan.Id,
-                value: event.target.value
-            }
-        });
-        this.dispatchEvent(typeEvent);
+    const typeEvent = new CustomEvent("typeselect", {
+      detail: {
+        plan: this.plan.Id,
+        value: event.target.value
+      }
+    });
+    this.dispatchEvent(typeEvent);
+  }
+
+  setButtonVariants() {
+    switch (this.type) {
+      case "Data":
+        this.dataButtonVariant = "Brand";
+        this.textButtonVariant = "Neutral";
+        this.talkButtonVariant = "Neutral";
+        break;
+      case "Text":
+        this.dataButtonVariant = "Neutral";
+        this.textButtonVariant = "Brand";
+        this.talkButtonVariant = "Neutral";
+        break;
+      case "Talk":
+        this.dataButtonVariant = "Neutral";
+        this.textButtonVariant = "Neutral";
+        this.talkButtonVariant = "Brand";
+        break;
+      default:
+        this.dataButtonVariant = "Brand";
+        this.textButtonVariant = "Neutral";
+        this.talkButtonVariant = "Neutral";
     }
-
-    setButtonVariants(){
-        switch(this.type) {
-            case 'Data':
-                this.dataButtonVariant = 'Brand';
-                this.textButtonVariant = 'Neutral';
-                this.talkButtonVariant = 'Neutral';
-                break;
-            case 'Text':
-                this.dataButtonVariant = 'Neutral';
-                this.textButtonVariant = 'Brand';
-                this.talkButtonVariant = 'Neutral';
-                break;
-            case 'Talk':
-                this.dataButtonVariant = 'Neutral';
-                this.textButtonVariant = 'Neutral';
-                this.talkButtonVariant = 'Brand';
-                break;
-            default:
-                this.dataButtonVariant = 'Brand';
-                this.textButtonVariant = 'Neutral';
-                this.talkButtonVariant = 'Neutral';
-        }
-    }
+  }
 }
