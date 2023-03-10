6f9dff57734febf81ade974128c2a06c92bfb6f5
Christopher Weil <cweil@salesforce.com>
2023-03-10 11:19:01 -0500

    preittified deviceCard jest

--- force-app/main/default/lwc/deviceCard/__tests__/deviceCard.test.js
+++ force-app/main/default/lwc/deviceCard/__tests__/deviceCard.test.js
@@ -23,20 +23,22 @@ describe("c-device-card", () => {
 
     // Assert
     await Promise.resolve();
-      const icon = element.shadowRoot.querySelector("lightning-icon");
-      const strongs = element.shadowRoot.querySelectorAll("strong");
-      const ps = element.shadowRoot.querySelectorAll("p");
-      const helptext = element.shadowRoot.querySelector("lightning-helptext");
-      expect(icon.src).toBe("UsagePhone#icon");
-      expect(strongs[0].textContent).toBe(testData.Phone.User__c);
-      expect(ps[0].textContent).toBe(testData.Phone.Phone_Number_Display__c);
-      expect(strongs[1].textContent).toBe(testData.Phone.Make_and_Model__c);
-      expect(ps[1].textContent).toContain(
-          "" + testData.Phone.Usage__r.Current_Usage__c
-      );
-      expect(ps[1].textContent).toContain(testData.Phone.Usage__r.Unit_of_Measure__c);
-      expect(ps[1].textContent).toContain("unlimited");
-      expect(helptext).toBe(null);
+    const icon = element.shadowRoot.querySelector("lightning-icon");
+    const strongs = element.shadowRoot.querySelectorAll("strong");
+    const ps = element.shadowRoot.querySelectorAll("p");
+    const helptext = element.shadowRoot.querySelector("lightning-helptext");
+    expect(icon.src).toBe("UsagePhone#icon");
+    expect(strongs[0].textContent).toBe(testData.Phone.User__c);
+    expect(ps[0].textContent).toBe(testData.Phone.Phone_Number_Display__c);
+    expect(strongs[1].textContent).toBe(testData.Phone.Make_and_Model__c);
+    expect(ps[1].textContent).toContain(
+      "" + testData.Phone.Usage__r.Current_Usage__c
+    );
+    expect(ps[1].textContent).toContain(
+      testData.Phone.Usage__r.Unit_of_Measure__c
+    );
+    expect(ps[1].textContent).toContain("unlimited");
+    expect(helptext).toBe(null);
   });
 
   it("Device Card for Tablet with Limited SMS", async () => {
@@ -52,20 +54,24 @@ describe("c-device-card", () => {
 
     // Assert
     await Promise.resolve();
-      const icon = element.shadowRoot.querySelector("lightning-icon");
-      const strongs = element.shadowRoot.querySelectorAll("strong");
-      const ps = element.shadowRoot.querySelectorAll("p");
-      const helptext = element.shadowRoot.querySelector("lightning-helptext");
-      expect(icon.src).toBe("UsageTablet#icon");
-      expect(strongs[0].textContent).toBe(testData.Tablet.User__c);
-      expect(ps[0].textContent).toBe(testData.Tablet.Phone_Number_Display__c);
-      expect(strongs[1].textContent).toBe(testData.Tablet.Make_and_Model__c);
-      expect(ps[1].textContent).toContain(
-          "" + testData.Tablet.Usage__r.Current_Usage__c
-      );
-      expect(ps[1].textContent).toContain(testData.Tablet.Usage__r.Unit_of_Measure__c);
-      expect(ps[1].textContent).toContain("" + testData.Tablet.Usage__r.Allotted_Usage__c);
-      expect(helptext.content).toBe("Over Limit");
+    const icon = element.shadowRoot.querySelector("lightning-icon");
+    const strongs = element.shadowRoot.querySelectorAll("strong");
+    const ps = element.shadowRoot.querySelectorAll("p");
+    const helptext = element.shadowRoot.querySelector("lightning-helptext");
+    expect(icon.src).toBe("UsageTablet#icon");
+    expect(strongs[0].textContent).toBe(testData.Tablet.User__c);
+    expect(ps[0].textContent).toBe(testData.Tablet.Phone_Number_Display__c);
+    expect(strongs[1].textContent).toBe(testData.Tablet.Make_and_Model__c);
+    expect(ps[1].textContent).toContain(
+      "" + testData.Tablet.Usage__r.Current_Usage__c
+    );
+    expect(ps[1].textContent).toContain(
+      testData.Tablet.Usage__r.Unit_of_Measure__c
+    );
+    expect(ps[1].textContent).toContain(
+      "" + testData.Tablet.Usage__r.Allotted_Usage__c
+    );
+    expect(helptext.content).toBe("Over Limit");
   });
 
   it("Device Card for Watch with Limited Minutes", async () => {
@@ -81,19 +87,23 @@ describe("c-device-card", () => {
 
     // Assert
     await Promise.resolve();
-      const icon = element.shadowRoot.querySelector("lightning-icon");
-      const strongs = element.shadowRoot.querySelectorAll("strong");
-      const ps = element.shadowRoot.querySelectorAll("p");
-      const helptext = element.shadowRoot.querySelector("lightning-helptext");
-      expect(icon.src).toBe("UsageWatch#icon");
-      expect(strongs[0].textContent).toBe(testData.Watch.User__c);
-      expect(ps[0].textContent).toBe(testData.Watch.Phone_Number_Display__c);
-      expect(strongs[1].textContent).toBe(testData.Watch.Make_and_Model__c);
-      expect(ps[1].textContent).toContain(
-          "" + testData.Watch.Usage__r.Current_Usage__c
-      );
-      expect(ps[1].textContent).toContain(testData.Watch.Usage__r.Unit_of_Measure__c);
-      expect(ps[1].textContent).toContain("" + testData.Watch.Usage__r.Allotted_Usage__c);
-      expect(helptext.content).toBe("Near Limit");
+    const icon = element.shadowRoot.querySelector("lightning-icon");
+    const strongs = element.shadowRoot.querySelectorAll("strong");
+    const ps = element.shadowRoot.querySelectorAll("p");
+    const helptext = element.shadowRoot.querySelector("lightning-helptext");
+    expect(icon.src).toBe("UsageWatch#icon");
+    expect(strongs[0].textContent).toBe(testData.Watch.User__c);
+    expect(ps[0].textContent).toBe(testData.Watch.Phone_Number_Display__c);
+    expect(strongs[1].textContent).toBe(testData.Watch.Make_and_Model__c);
+    expect(ps[1].textContent).toContain(
+      "" + testData.Watch.Usage__r.Current_Usage__c
+    );
+    expect(ps[1].textContent).toContain(
+      testData.Watch.Usage__r.Unit_of_Measure__c
+    );
+    expect(ps[1].textContent).toContain(
+      "" + testData.Watch.Usage__r.Allotted_Usage__c
+    );
+    expect(helptext.content).toBe("Near Limit");
   });
 });
